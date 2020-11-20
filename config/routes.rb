73f5729f6558eca43af120ae1e6cfc2e17df3a40
Rob Flynn <rob@thingerly.com>
2020-11-19 23:27:24 -0500

    Scope the api routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :assets
-  resources :stores do
-    collection do
-      resources :stores, path: 'local', as: 'local_store', type: 'Store::LocalStore', controller: 'stores'
-    end
-  end
+  scope :api do
+    resources :assets, path: 'assets'
 
-  resources :encoding_tasks, path: 'tasks/encodes', as: 'encoding'
+    resources :stores do
+      collection do
+        resources :stores, path: 'local', as: 'local_store', type: 'Store::LocalStore', controller: 'stores'
+      end
+    end
+
+    # Tasks
+    resources :encoding_tasks, path: 'tasks/encodes', as: 'encoding'
+  end
 end
