ba74ea85648c35d8b672c5b22bce827f293a4ca5
Rob Flynn <rob@thingerly.com>
2020-11-20 00:30:04 -0500

    Add factory bot.

--- spec/controllers/stores_controller_spec.rb
+++ spec/controllers/stores_controller_spec.rb
@@ -1,22 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe StoresController, type: :controller do
+    describe "Stores API", type: :request do
+      it 'returns all stores' do
+        FactoryBot.create(:store, name: 'Local Store', type: 'Store::LocalStore', configuration: { base_path: 'test' } )
 
-    let(:valid_attributes) {
-        { type: 'Store::LocalStore', name: 'Local Test Store', configuration: "{ base_path: 'test' }" }
-    }
+        get '/api/stores', as: :json
 
-    let(:valid_session) { {} }
-
-    before :each do
-      request.headers["accept"] = 'application/json'
-    end
-
-    describe "GET #index" do
-        it "returns a success response" do
-            Store.create! valid_attributes
-            get :index, params: {}, session: valid_session
-            expect(response).to be_successful
-        end
+        expect(response).to have_http_status(:success)
+      end
     end
 end
