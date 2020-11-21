74e759e57bb0ff839779e30fe1ff68aeb8f4645c
Rob Flynn <rob@thingerly.com>
2020-11-20 23:05:22 -0500

    Auto-make routes for stores.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
 
     resources :stores do
       collection do
-        resources :stores, path: 'local', as: 'local_store', type: 'Store::LocalStore', controller: 'stores'
+
+        Store.subclasses.map(&:name).each do |store_class|
+          store_class_suffix = store_class.split("::")[1..].join("::").underscore
+          store_type = store_class_suffix.split('_')[0...-1].join('_')
+          resources :stores, path: store_type, as: store_class_suffix, type: store_class, controller: 'stores'
+        end
       end
     end
 
